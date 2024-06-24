7e38f402f1299d49e5c96b7792d9fae1ac7de98b
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-06-24 16:34:59 +0200

    style dog list

--- app/components/DogList.tsx
+++ app/components/DogList.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Image } from "@chakra-ui/react";
+import { Box, Flex, GridItem, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import { Dog } from "@prisma/client";
 
 interface Props {
@@ -8,12 +8,49 @@ interface Props {
 export function DogList(props: Props) {
   return (
     <Box>
-      {props.dogs.map((dog) => (
-        <Box>
-          <Heading>{dog.name}</Heading>
-          <Image src={dog.image || undefined} />
-        </Box>
-      ))}
+      <SimpleGrid
+        id="products-grid"
+        width="80%"
+        m="2rem auto"
+        mt="5rem"
+        columns={{ base: 1, md: 3, lg: 4 }}
+        gap={5}
+      >
+        {props.dogs.map((dog) => (
+          <GridItem
+            key={dog.id}
+            boxShadow="1px 1px 2px rgba(0,0,0,0.1)"
+            transition={"transform 0.2s ease-in-out"}
+            _hover={{ cursor: "pointer", transform: "scale(1.05)" }}
+          >
+            <Flex flexDirection="column" height="100%">
+              <Box _hover={{ textDecoration: "none" }}>
+                <Image
+                  src={dog.image || undefined}
+                  alt={dog.name}
+                  objectFit="cover"
+                  width="100%"
+                  height="200px"
+                />
+                <Flex
+                  justifyContent="flex-start"
+                  alignContent="center"
+                  flexDirection="column"
+                  mt="1.5rem"
+                >
+                  <Text
+                    fontWeight="semibold"
+                    textTransform="capitalize"
+                    _hover={{ color: "brown" }}
+                  >
+                    {dog.name}
+                  </Text>
+                </Flex>
+              </Box>
+            </Flex>
+          </GridItem>
+        ))}
+      </SimpleGrid>
     </Box>
   );
 }
