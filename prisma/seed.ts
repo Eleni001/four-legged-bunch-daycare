7e0c438214eb4420c24af000bb195515c16a44b7
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2025-04-09 21:41:05 +0200

    schema update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,7 @@
 import { db } from "./db";
 
 async function main() {
+  await db.dog.deleteMany();
   await db.owner.deleteMany();
   await db.owner.create({
     data: {
@@ -13,7 +14,8 @@ async function main() {
             name: "Oscar Jr.",
             breed: "Miniture Schnauzer",
             age: 4,
-            image: "https://media.istockphoto.com/id/802317706/photo/sweet-dog-schnautzer.webp?b=1&s=170667a&w=0&k=20&c=q3UjzWFtub-Jrp_3JVEdnaJ7t2O1eWRifEI4Jcbnlkc=",
+            image:
+              "https://media.istockphoto.com/id/802317706/photo/sweet-dog-schnautzer.webp?b=1&s=170667a&w=0&k=20&c=q3UjzWFtub-Jrp_3JVEdnaJ7t2O1eWRifEI4Jcbnlkc=",
             isVaccinated: true,
             neutered: false,
           },
@@ -32,7 +34,8 @@ async function main() {
             name: "Tore",
             breed: "Tax",
             age: 7,
-            image: "https://images.unsplash.com/photo-1605638868237-0660a05a135b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image:
+              "https://images.unsplash.com/photo-1605638868237-0660a05a135b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             isVaccinated: true,
             neutered: false,
           },
@@ -51,10 +54,11 @@ async function main() {
             name: "Pixy",
             breed: "Miniture Poodle",
             age: 2,
-            image: "https://images.unsplash.com/photo-1645097140485-ccf3d61d60d5?q=80&w=2912&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image:
+              "https://images.unsplash.com/photo-1645097140485-ccf3d61d60d5?q=80&w=2912&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             isVaccinated: true,
             neutered: true,
-            isCheckedIn: true
+            isCheckedIn: true,
           },
         ],
       },
@@ -71,7 +75,8 @@ async function main() {
             name: "Della",
             breed: "Miniture Schnauzer",
             age: 5,
-            image: "https://images.unsplash.com/photo-1642546007057-6f4c6e6e2b34?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image:
+              "https://images.unsplash.com/photo-1642546007057-6f4c6e6e2b34?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             isVaccinated: true,
             neutered: false,
           },
@@ -90,7 +95,8 @@ async function main() {
             name: "Buster",
             breed: "French bulldog",
             age: 2,
-            image: "https://images.unsplash.com/photo-1582656400825-71c718be1584?q=80&w=3062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image:
+              "https://images.unsplash.com/photo-1582656400825-71c718be1584?q=80&w=3062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             isVaccinated: true,
             neutered: true,
           },
